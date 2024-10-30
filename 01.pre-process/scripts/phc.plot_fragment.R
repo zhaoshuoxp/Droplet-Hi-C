236b2626facdc5483089a48901fd91c80eafe8a3
Yang Xie <y2xie@silencer.sdsc.edu>
2024-10-29 19:32:51 -0700

    241029: add scripts for pre-processing

---
+++ 01.pre-process/scripts/phc.plot_fragment.R
@@ -0,0 +1,51 @@
+
+suppressPackageStartupMessages(library(R.utils))
+suppressPackageStartupMessages(library(data.table))
+suppressPackageStartupMessages(library(dplyr))
+library(ggplot2)
+library(scales)
+library(grid)
+
+args <- commandArgs(trailingOnly = TRUE)
+pairs <- args[1] # .pairs or .pairs to plot
+pairs_name <- basename(pairs)
+if (substr(pairs, nchar(pairs)-1, nchar(pairs)) == "gz") {
+	# skip <- system(paste0("zcat ", pairs, " | grep '^#' - | wc -l"), intern = T)
+	system(paste0("zcat ", pairs, " | awk '{if(substr($1,1,1)==\"#\"){next}{print}}' - | awk 'BEGIN {srand()} !/^$/ {if (rand() <= .001) print $0}' > ", pairs, ".tmp"))
+} else {
+	# skip <- system(paste0("grep '^#' ", pairs, " | wc -l"), intern = T)
+	system(paste0("cat ", pairs, " | awk '{if(substr($1,1,1)==\"#\"){next}{print}}' - | awk 'BEGIN {srand()} !/^$/ {if (rand() <= .001) print $0}' > ", pairs, ".tmp"))
+}
+opairs <- fread(paste0(pairs, ".tmp"), sep="\t", header = F) #, skip = as.numeric(skip))
+opairs <- opairs[opairs$V2 != "*" & opairs$V4 != "*", ]
+total <- nrow(opairs)
+
+if (dim(opairs)[2] < 10){
+	warning("Barcodeds columns not detected in Hi-C pairs. Summarize fragments regardless of barcodes identity.\n", immediate. = T, call. = F)
+} else {
+	inter_cells <- length(which(opairs$V9 != opairs$V10))
+	opairs <- opairs[which(opairs$V9 == opairs$V10),]
+}
+trans <- length(which(opairs$V2 != opairs$V4))
+opairs <- opairs[which(opairs$V2 == opairs$V4),]
+flen.median <- median(opairs$V5 - opairs$V3)
+flen.mean <- mean(opairs$V5 - opairs$V3)
+flen.25 <- quantile((opairs$V5 - opairs$V3), probs = 0.25)
+flen.75 <- quantile((opairs$V5 - opairs$V3), probs = 0.75)
+flen.max <- max(opairs$V5 - opairs$V3)
+# my_anno = grid.text(paste0("sample:", pairs, "\n", "total pairs: ", total, "\n", "trans pair: ", trans, "\n", "meidna fragment size: ", flen.median, "\n", "mean fragment size: ", flen.mean), 
+# 	x=Inf, y = Inf, vjust=1, hjust=1,
+# 	gp=gpar(col="#c3533d", fontsize=10, fontface="bold"))
+
+flen.hist <- as.data.frame(opairs$V5 - opairs$V3) %>% rename("flen" = 1) %>%
+ggplot(aes(x=flen+1)) +
+geom_histogram(bins=50, fill="#477a96", color="white") + 
+xlab("Fragment length (bp)") + 
+ylab("Fragment counts") + 
+scale_x_log10(limits = c(1,10^8), labels = trans_format("log10", math_format(10^.x))) +
+theme_bw() + 
+annotate("text", label = paste0("Sample: ", pairs_name, "\n", "Median fragment size: ", as.integer(flen.median), "\n", "Mean fragment size: ", as.integer(flen.mean), "\n", "25% Quantile: ", flen.25, "\n", "75% Quantile: ", flen.75), 
+	x = max(flen.max)/10, y = Inf, vjust = 1.5, hjust = 0)
+
+ggsave(flen.hist, filename = paste0(pairs, ".fragmentLen.png"), dpi = 300, width = 16, height = 8)
+# system(paste0("rm ", pairs, ".tmp"))
---
+++ 01.pre-process/scripts/phc.summarize_pairs_lec.R
@@ -0,0 +1,28 @@
+
+suppressPackageStartupMessages(library(R.utils))
+suppressPackageStartupMessages(library(data.table))
+suppressPackageStartupMessages(library(dplyr))
+
+args <- commandArgs(trailingOnly = TRUE)
+pairs <- args[1] # .pairparse.txt or .pairdedup.txt to summarize
+pname <- substr(pairs, 0, nchar(pairs) - 4)
+
+tmp <- read.table(paste0(pairs), header = F, sep = "\t", row.names = 1)
+tmp1 <- tmp %>% filter(row.names(tmp) %in% c("total", "total_unmapped", "total_single_sided_mapped", "total_mapped", "total_dups", "total_nodups", "cis_1kb+", "cis_20kb+", "cis", "trans")) %>% 
+t() %>% as.data.frame() %>%
+mutate(Sequenced_Read_Pairs = sum(total_unmapped, total_single_sided_mapped, total_dups, total_nodups),
+      Mapped_Read_Pairs = sum(total_dups, total_nodups),
+      Nonclonal_Read_Pairs = total_nodups,
+      Intra_chromosomal = cis,
+      short_range_1k = cis - `cis_1kb+`,
+      long_range_1k = `cis_1kb+`,
+      Inter_chromosomal = trans,
+      Unmapped = sum(total_unmapped, total_single_sided_mapped),
+      Removed_Duplicates = total_dups) %>% 
+select(c(11:19)) %>% 
+t() %>% as.data.frame() %>%
+mutate(ratio = ifelse(row_number() %in% c(1,2,8), V2/V2[[1]], V2/V2[[3]])) %>%
+mutate(ratio = ifelse(row_number() %in% c(3,9), V2/V2[[2]], ratio)) %>%
+mutate(ratio = ifelse(row_number() %in% c(5,6), V2/V2[[4]], ratio))
+
+write.table(tmp1, file = paste0(pname,".summary.txt"), row.names = T, col.names = F, sep = "\t", quote = F)
